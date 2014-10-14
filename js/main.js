8c227e358201417c1a3e7e172c0f6a2f7e126269
Steven Senkus <steven.senkus@yahoo.com>
2014-10-14 15:25:44 -0700

    Handlebars templates and refactoring

--- js/main.js
+++ js/main.js
@@ -1,34 +1,20 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
-
-    showAlert: function(message, title) {
+    showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK!!!');
         } else {
             alert(title ? (title + ": " + message) : message);
         }
-        
-    },
 
-    initialize: function() {
+    },
+    initialize: function () {
         var self = this;
-        this.store = new MemoryStore(function() {
-            self.showAlert('Store Initialized', 'Info');
+        this.store = new MemoryStore(function () {
+           $('body').html(new HomeView(self.store).render().el); // self.renderHomeView();
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-        
+
+
     }
 
 };
